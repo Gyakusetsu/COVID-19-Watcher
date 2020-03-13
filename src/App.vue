26ea2e27f6dd06e1c2405e555ed7c58286d38a1e
Gyakusetsu <reymarbc@gmail.com>
2020-03-14 01:25:30 +0800

    Fixed async map

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   name: "App",
   async mounted() {
     var mymap = L.map("mapid").setView([51.505, -0.09], 2);
-    L.tileLayer(
+    await L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
         // attribution:   'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
